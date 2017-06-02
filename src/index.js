713784b35f68c131d478618c1a2562372aa056b5
ml2439 <ml2439@cornell.edu>
2017-06-02 16:36:11 -0400

    Use state for Counter and remove its props

--- src/index.js
+++ src/index.js
@@ -34,27 +34,31 @@ Header.propTypes = {
 }
 
 class Counter extends Component {
+    constructor() {
+        super();
+        this.state = {
+            score: 0
+        }
+    }
     render() {
         return (
             <div className="counter">
                 <button className="counter-action decrement"> - </button>
-                <div className="counter-score">{this.props.score}</div>
+                <div className="counter-score">{this.state.score}</div>
                 <button className="counter-action increment"> + </button>
             </div>           
         );
     }
 }
 
-Counter.propTypes = {
-    score: PropTypes.number.isRequired
-}
+Counter.propTypes = {}
 
 function Player(props) {
     return (
         <div className="player">
             <div className="player-name">{props.name}</div>
             <div className="player-score">
-                <Counter score={props.score} />
+                <Counter />
             </div>
         </div>        
     );
